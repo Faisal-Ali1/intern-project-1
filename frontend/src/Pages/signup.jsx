dadfa38c590f4c03d454e397e2e2a1cc542a2a94
Faisal Ali <fali77616@gmail.com>
2025-08-07 17:54:12 +0530

    making signupPage

--- frontend/src/Pages/signup.jsx
+++ frontend/src/Pages/signup.jsx
@@ -1,7 +1,60 @@
+import { useForm } from "react-hook-form";
+import { NavLink } from "react-router";
+
 function SignUpPage(){
+
+     const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const submitData = (data) => {
+        console.log(data);
+    }
+
     return(
         <>
-        Signup page
+        <div className="bg-amber-100 h-[100vh] flex justify-center items-center">
+            <div className="shadow-2xl rounded-2xl p-10 ">
+                 <form onSubmit={handleSubmit(submitData)} className='flex flex-col gap-5'>
+                
+                                        {/* username */}
+                                        <div className='flex flex-col gap-1'>
+                                            <label className='label'>UserName</label>
+                                            <input
+                                                type="text"
+                                                className='input w-80'
+                                                placeholder='bob'
+                                                required = {true}
+                                                {...register('username', { required: true })} />
+                                        </div>
+
+                                        {/* Email */}
+                                        <div className='flex flex-col gap-1'>
+                                            <label className='label'>Email</label>
+                                            <input
+                                                type="email"
+                                                className='input w-80'
+                                                placeholder='bob@gmail.com'
+                                                required = {true}
+                                                {...register('email', { required: true })} />
+                                        </div>
+                
+                                        {/* Password */}
+                                        <div className='flex flex-col gap-1'>
+                                            <label className='label'>password</label>
+                                            <input
+                                                type="password"
+                                                className='input w-80'
+                                                placeholder='***'
+                                                required = {true}
+                                                {...register('Password', { required: true })} />
+                                        </div>
+                
+                                        {/* submit button */}
+                                        <button className='btn btn-primary mt-3'>Submit</button>
+                                        <p className='text-center'>have account?<NavLink to={'/'} className=" text-blue-400"> signIn</NavLink></p>
+                
+                                    </form>
+            </div>
+        </div>
         </>
     )
 }
